aada4c0aa270547686facb9308aa93ffbc324d0d
quaternioneer <nat.admin@protonmail.com>
2020-12-13 20:32:49 -0800

    Append fstab, so that swap persists after rebooting.

--- terraform/provision-urbit.sh
+++ terraform/provision-urbit.sh
@@ -17,12 +17,25 @@ sudo /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=2048
 sudo /sbin/mkswap /var/swap.1
 sudo chmod 600 /var/swap.1
 sudo /sbin/swapon /var/swap.1
+#sudo echo "swap        /var/swap.1 swap    defaults        0   0" >> /etc/fstab
+# After >>, the above line does not run as sudo; it can't append fstab. Hence, this hack:
 echo "/var/swap.1 none	swap    defaults        0   0" | sudo tee -a /etc/fstab
 
 # Set up forwarding from port 80 (http) to 8080 and 443 (https) to 8443
 #IPtables doesn't persist after rebooting; fix this.
 sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j DNAT --to-destination :8080
 sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 443 -j DNAT --to-destination :8443
+sudo mkdir /etc/iptables
+sudo touch /etc/iptables/rules.v4
+sudo touch /etc/iptables/rules.v6
+
+#sudo /sbin/iptables-save > /etc/iptables/rules.v4
+## IPv6 ##
+#sudo /sbin/ip6tables-save > /etc/iptables/rules.v6
+# Again the same hack as above.
+
+sudo /sbin/iptables-save | sudo tee -a /etc/iptables/rules.v4
+sudo /sbin/iptables-save | sudo tee -a /etc/iptables/rules.v6
 
 # Other packages that are nice to have on the server
-sudo apt install -y expect git tmux iptables-persistent
+sudo apt install -y expect git tmux
