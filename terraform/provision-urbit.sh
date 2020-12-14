f79d2cd6a31575fddccf04f044156d49014f9230
quaternioneer <nat.admin@protonmail.com>
2020-12-13 17:30:06 -0800

    Append fstab, so that swap persists after rebooting.

--- terraform/provision-urbit.sh
+++ terraform/provision-urbit.sh
@@ -6,7 +6,7 @@ sudo apt install -y screen automake autoconf libtool cmake
 
 # Download build, and install the urbit source package
 cd ~
-mkdir source piers
+mkdir source
 cd source
 wget --content-disposition https://urbit.org/install/linux64/latest
 tar zxvf ./linux64.tgz --strip=1
@@ -17,11 +17,7 @@ sudo /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=2048
 sudo /sbin/mkswap /var/swap.1
 sudo chmod 600 /var/swap.1
 sudo /sbin/swapon /var/swap.1
-#sudo echo "swap        /var/swap.1 swap    defaults        0   0" >> /etc/fstab
-#The above line does not perform >> /etc/fstab as root, so I have this hack:
-sudo su root
-echo "/var/swap.1 none	swap    defaults        0   0" >> /etc/fstab
-exit
+echo "/var/swap.1 none	swap    defaults        0   0" | sudo tee -a /etc/fstab
 
 # Set up forwarding from port 80 (http) to 8080 and 443 (https) to 8443
 #IPtables doesn't persist after rebooting; fix this.
@@ -29,4 +25,4 @@ sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j DNAT --to-destin
 sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 443 -j DNAT --to-destination :8443
 
 # Other packages that are nice to have on the server
-sudo apt install -y expect git tmux
+sudo apt install -y expect git tmux iptables-persistent
