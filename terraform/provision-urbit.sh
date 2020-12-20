86a89728a8a6bbe3fc69036ca5ec24813a1d563c
quaternioneer <nat.admin@protonmail.com>
2020-12-19 20:06:56 -0800

    After rebooting, iptables persists now.

--- terraform/provision-urbit.sh
+++ terraform/provision-urbit.sh
@@ -3,11 +3,11 @@ sudo yum update -y
 
 # Install all urbit dependencies
 #sudo yum --enablerepo epel install -y gcc gcc-c++ gmp-devel openssl-devel ncurses-devel libsigsegv-devel ctags automake autoconf libtool cmake re2c libcurl-devel
-sudo yum install -y wget
+sudo yum install -y wget iptables iptables-services
 
 # Download build, and install the urbit source package
 cd ~
-mkdir source
+mkdir source piers
 cd source
 wget --content-disposition https://urbit.org/install/linux64/latest
 tar zxvf ./linux64.tgz --strip=1
@@ -18,25 +18,19 @@ sudo /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=2048
 sudo /sbin/mkswap /var/swap.1
 sudo chmod 600 /var/swap.1
 sudo /sbin/swapon /var/swap.1
-sudo echo "swap        /var/swap.1 swap    defaults        0   0" >> /etc/fstab
+#sudo echo "swap        /var/swap.1 swap    defaults        0   0" >> /etc/fstab
 # After >>, the above line does not run as sudo; it can't append fstab. Hence, this hack:
-#echo "/var/swap.1 none	swap    defaults        0   0" | sudo tee -a /etc/fstab
+echo "/var/swap.1 none	swap    defaults        0   0" | sudo tee -a /etc/fstab
 
 # Set up forwarding from port 80 (http) to 8080 and 443 (https) to 8443
 #IPtables doesn't persist after rebooting; fix this.
 sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j DNAT --to-destination :8080
 sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 443 -j DNAT --to-destination :8443
-#sudo mkdir /etc/iptables
-#sudo touch /etc/iptables/rules.v4
-#sudo touch /etc/iptables/rules.v6
 
-#sudo /sbin/iptables-save > /etc/iptables/rules.v4
-## IPv6 ##
-#sudo /sbin/ip6tables-save > /etc/iptables/rules.v6
-# Again the same hack as above.
-
-#sudo /sbin/iptables-save | sudo tee -a /etc/iptables/rules.v4
-#sudo /sbin/iptables-save | sudo tee -a /etc/iptables/rules.v6
+#sudo /sbin/iptables-save > /etc/sysconfig/iptables
+# Again, the same hack as above, for the same reason.
+sudo /sbin/iptables-save | sudo tee /etc/sysconfig/iptables
+sudo systemctl enable iptables
 
 # Other packages that are nice to have on the server
-sudo yum install -y expect git tmux
+sudo yum install -y expect git tmux vim
